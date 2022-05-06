15995df14a487e3569467b19456f092d6bba02f4
Kanishka Samarathunga <kanishkakcs@gmail.com>
2022-05-06 19:34:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hi there!</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
